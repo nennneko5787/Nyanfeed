dac181ef4ad7ac34dbf072dc3455c8d5745de745
ねんねこ <nennneko5787@gmail.com>
2024-08-05 11:28:03 +0900

    exceptions.py を更新

--- app/objects/exceptions.py
+++ app/objects/exceptions.py
@@ -1,8 +1,8 @@
 class BoardNotFoundError(Exception):
-    def __init__(self, message):
+    def __init__(self, message=""):
         super().__init__(message)
 
 
 class UnauthorizedFileExtensionError(Exception):
-    def __init__(self, message):
+    def __init__(self, message=""):
         super().__init__(message)
