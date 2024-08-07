705f98ed3c28566693c1c60dfe5c69cc0643b336
Nennneko5787 <nennneko5787@gmail.com>
2024-08-07 18:59:54 +0900

    aaaaaaaaaaaaaaaaaaaaaa

--- app/objects/websocket.py
+++ app/objects/websocket.py
@@ -6,6 +6,8 @@ from .user import User
 
 
 class WebSocket(fastapi.WebSocket):
+    nfuser: Optional[User] = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         nfuser: Optional[User] = None
